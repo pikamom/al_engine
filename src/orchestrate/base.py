c191c296ebab6fbbd5166b6c74fcbe85b9f046a8
xgzz9 <ucei038@ucl.ac.uk>
2023-07-30 02:15:49 +0100

    first attempt to add in click to the orchestrate module

--- src/orchestrate/base.py
+++ src/orchestrate/base.py
@@ -1,10 +1,13 @@
 from abc import abstractmethod
-
+import click
 
 class Orchestrate:
     def __init__(self, list_modules: list):
         self.list_modules = list_modules
 
+    @click.command()
+    @click.argument('module_name',type=str)
     @abstractmethod
-    def run(self):
+    def run(self, module_name):
+        print(module_name)
         pass
