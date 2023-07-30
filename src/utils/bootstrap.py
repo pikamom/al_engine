6ccf253f33dd14b53780d2cda3635e3e8836d636
xgzz9 <ucei038@ucl.ac.uk>
2023-07-30 03:36:59 +0100

    complete the boostrap step, add in run meta data creation

--- src/utils/bootstrap.py
+++ src/utils/bootstrap.py
@@ -1,11 +1,39 @@
 from src.utils.logger import LoggerSetup
+from datetime import datetime
+import os
+import uuid
 import logging
 
 logger=logging.getLogger()
+LoggerSetup()
 
 def Boostrap():
-    LoggerSetup()
-
+    logger.debug("Starting the boostrap task...")
     
+    # run meta data
+    run_id=uuid.uuid4()
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    today_date=datetime.now().strftime("%Y%m%d")
+
+    logger_message=f"""
+    ###########################
+
+    Boostrap task im progress...
+    run id: {run_id}
+    run time: {time_now}
+    run folder created at: runs/{today_date}/{run_id}
+
+    ###########################
+    """
+
+    # log the run meta data
+    logger.info(logger_message)
+
+    # make a run folder
+    logger.info(f"Creating the run folder at [runs/{today_date}/{run_id}]")
+    os.makedirs(f"runs/{today_date}/{run_id}")
+    
+    logger.debug("Boostrap task completed successfully!")
+
 
 
