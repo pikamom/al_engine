c92229cbbcbb20d3ca68a3862dbf6e13cbc69f07
xgzz9 <ucei038@ucl.ac.uk>
2023-07-30 01:56:37 +0100

    add in a staticmethod for csv saving util

--- src/utils/saver.py
+++ src/utils/saver.py
@@ -3,8 +3,18 @@ import logging
 
 logger=logging.getLogger()
 
-class Saver:
-    def __init__(self, df_to_save: pd.DataFrame) -> None:
-        df_to_save.ti
 
+class Saver:
+    def __init__(self) -> None:
         pass
+
+    @staticmethod
+    def save_csv(df_to_save: pd.DataFrame, filename: str):
+
+        if filename[-4:] != ".csv":
+            logger.debug("The filename does not contain .csv as file extension, adding .csv...")
+            filename=filename+".csv"
+        
+        logger.debug(f"Saving file with file name [{filename}] at location [src/data/{filename}]...")
+        df_to_save.to_csv(f"src/data/{filename}")
+        logger.info("Save successful!")
