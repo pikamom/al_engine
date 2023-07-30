30b3711357d8e8cca33f64b8913a1db9a858786b
xgzz9 <ucei038@ucl.ac.uk>
2023-07-30 05:11:10 +0100

    add in save plots functionality

--- src/utils/saver.py
+++ src/utils/saver.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import logging
+import matplotlib.pyplot as plt
+from src.utils.settings import SETTINGS
 
 logger = logging.getLogger("al_engine")
 
@@ -20,4 +22,18 @@ class Saver:
             f"Saving file with file name [{filename}] at location [data/{filename}]..."
         )
         df_to_save.to_csv(f"data/{filename}")
+        logger.info("Save successful!")
+
+    @staticmethod
+    def save_plots(filename: str, extension: str=".png"):
+        if filename[-4:] != extension:
+            logger.debug(
+                f"The filename does not contain specified extension, adding {extension}..."
+            )
+            filename = filename + extension
+  
+        logger.debug(
+            f'Saving file with file name [{filename}] at location [{SETTINGS["run_meta_data"]["run_folder"]}/plots]...'
+        )
+        plt.savefig(f'{SETTINGS["run_meta_data"]["run_folder"]}/plots/{filename}', dpi=300)
         logger.info("Save successful!")
