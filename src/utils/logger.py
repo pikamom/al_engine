a4c2eb4a9693c116d2a6d7959b436a1b9a081269
xgzz9 <ucei038@ucl.ac.uk>
2023-07-30 03:16:31 +0100

    modify the logger set up file

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -6,9 +6,12 @@ from colorlog import ColoredFormatter
 
 class LoggerSetup:
     def __init__(self) -> None:
+
         logger = logging.getLogger()
+
         # add in console output handler
         console_output = logging.StreamHandler(sys.stderr)
+
         # formatter
         formatter = ColoredFormatter(
             "%(log_color)s[%(levelname)s][%(filename)s][%(asctime)s] %(reset)s%(white)s%(message)s",  # noqa
@@ -22,10 +25,9 @@ class LoggerSetup:
                 "CRITICAL": "red",
             },
         )
+
         console_output.setFormatter(formatter)
+        
         # add in console output handler
         logger.addHandler(console_output)
         logger.setLevel(logging.DEBUG)
-
-
-LoggerSetup()
