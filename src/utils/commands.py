44fa035a8449e0d138f6f41181912d1908912ab3
xgzz9 <ucei038@ucl.ac.uk>
2023-07-30 03:09:42 +0100

    complete the orchestrator commands

---
+++ src/utils/commands.py
@@ -0,0 +1,36 @@
+import importlib
+import importlib.util
+import yaml
+import click
+import logging
+
+logger=logging.getLogger()
+
+class Orchestrator:
+
+    @click.command()
+    @click.argument('orc',type=str)
+    def run_orchestartion(orc):
+
+        logger.info("Loading in config files from src/config.yaml...")
+        with open("src/config.yaml","r") as file:
+            settings=yaml.safe_load(file)
+
+        logger.info("Reading orchestration file path")
+        paths_to_orchestrate=f'{settings["orchestration"]["folder_path"]}/{orc}.py'
+        logger.debug(f"Using [{paths_to_orchestrate}] file to obtain a list of modules")
+
+        logger.debug("Loading the [list_modules] variable from orchestration file...")
+        module_spec = importlib.util.spec_from_file_location(orc, paths_to_orchestrate)
+        orchestrator_file = importlib.util.module_from_spec(module_spec)
+        module_spec.loader.exec_module(orchestrator_file)
+
+        logger.info("Loading is successful!")
+        list_modules_name=[module.__name__ for module in orchestrator_file.list_modules]
+        logger.debug(f"The list of modules that will be executed are {list_modules_name}")
+        
+        logger.info("Starting all modules execution...")
+        for module in orchestrator_file.list_modules:
+            logger.info("Queueing modules execution for module [{module}]...")
+            module().run()
+        logger.debug("All modules execution complete and is successful!")
