dea55dcedd49492f93749072474917144f9bf42e
xgzz9 <hybingcarol@gmail.com>
2023-08-03 00:56:56 +0100

    add in eda for corrrelation

--- src/modules/analysis/eda.py
+++ src/modules/analysis/eda.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime, timedelta
 from typing import Dict
-
+import seaborn as sns
 import matplotlib.pyplot as plt
 import missingno as msno
 import pandas as pd
@@ -10,7 +10,7 @@ import requests
 from matplotlib.pyplot import figure
 import plotly.express as px
 from sklearn.preprocessing import MinMaxScaler
-
+from statsmodels.tsa.stattools import adfuller
 from src.modules.base import Module
 from src.utils.saver import Saver
 
@@ -25,13 +25,37 @@ class EDA(Module):
     def run(self):
         logger.info("Reading in cleaned dataframe")
         clean_df=pd.read_csv("data/processed/cleaned_data.csv")
-        merged_scaled=pd.read_csv("data/processed/scaled_cleaned_data.csv")
-
-
-        print(merged_scaled)
+        merged_scaled=pd.read_csv("data/processed/scaled_cleaned_data.csv").set_index("DATE") # change name later on
 
         logger.info("Giving a line plot on all features")
-        fig = px.line(merged_scaled.drop('DATE',axis=1), facet_col="variable", facet_col_wrap=3,
+        logger.warn("Need to fix the plot saving")
+        fig = px.line(merged_scaled, facet_col="variable", facet_col_wrap=3,
                     width=1000, height=1200, facet_row_spacing=0.02)
-        fig.show()
         Saver.save_plots("line_plot")
+        plt.clf()
+
+        logger.info("Giving a correlation plot on all features")
+        corr_matrix = merged_scaled.corr()
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax_heatmap = sns.heatmap(
+            corr_matrix, 
+            vmin=-1, vmax=1, center=0,
+            cmap=sns.diverging_palette(20, 170, n=200),
+            square=True
+        )
+        ax_heatmap.set_xticklabels(
+            ax_heatmap.get_xticklabels(),
+            rotation=90,
+            horizontalalignment='right'
+        )
+        Saver.save_plots("correlation_plot")
+        plt.clf()
+
+        logger.info("Outputing actual correlation to logging")
+        corr_info=corr_matrix["AL_PRICE"].sort_values(ascending=False)
+        logger.info(f"""Correlation info: [
+                    {corr_info}
+                    ]""")
+
+
+        
