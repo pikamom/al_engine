2264452fbc54761439168a7f40ec499003c55dc5
xgzz9 <hybingcarol@gmail.com>
2023-08-03 00:26:25 +0100

    add in minmax scaler

--- src/modules/data_processing/scaling.py
+++ src/modules/data_processing/scaling.py
@@ -9,6 +9,8 @@ import pandas as pd
 import requests
 from matplotlib.pyplot import figure
 
+from sklearn.preprocessing import MinMaxScaler
+
 from src.modules.base import Module
 from src.utils.saver import Saver
 
@@ -21,4 +23,22 @@ class Scaling(Module):
         super().__init__(module_name)
 
     def run(self):
+        logger.info("Reading in cleaned dataframe")
         raw_df=pd.read_csv("data/processed/cleaned_data.csv")
+
+        merged_11 = raw_df
+        logger.info("Starting min-max scaling process")
+        scaler = MinMaxScaler()
+
+        logger.debug("Fit and transform the explainary variables dataframe with the scaler")
+        merged_copy=merged_11.copy()
+        merged_copy=merged_copy.set_index('DATE')
+        merged_scaled = pd.DataFrame(scaler.fit_transform(merged_copy), columns=merged_copy.columns)
+        merged_scaled['DATE']=merged_11['DATE']
+
+        print(merged_scaled.head(20))
+
+        logger.debug("Dropping rows with missing values as a result of RSI calculation")
+        merged_scaled=merged_scaled.dropna(subset=["RSI"])
+
+        print(merged_scaled.head(20))
