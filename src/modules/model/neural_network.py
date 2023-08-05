f5885de931bb1ea65500a213cada48e16a9ada23
xgzz9 <hybingcarol@gmail.com>
2023-08-06 04:33:27 +0800

    complete lstm

--- src/modules/model/neural_network.py
+++ src/modules/model/neural_network.py
@@ -26,27 +26,86 @@ class NeuralNetworkModel(Module):
         logger.info("Reading in training datasets from unshifted data")
         clean_scaled_data = pd.read_csv("data/processed_scaled_clean_data.csv")
 
-        logger.debug("Getting the training and testing data")
-        X_train, y_train, X_test, y_test = self._get_data(
-            self.settings["model"]["network_model"]["cut_off_trading_day"],
-            self.settings["model"]["network_model"]["prediction_num_days"],
-            clean_scaled_data,
+        logger.debug("Running the LSTM model for difference structure")
+        lstm_y_pred_test = []
+        for model_type in [1, 2, 3]:
+            for num_interval, incremental_trading_day in enumerate(
+                np.arange(
+                    0,
+                    631,
+                    self.settings["model"]["network_model"]["prediction_num_days"],
+                )
+            ):
+                logger.info("Get the cut of trading day")
+                cut_off_trading_day = (
+                    self.settings["model"]["network_model"]["start_cut_off_trading_day"]
+                    + incremental_trading_day
+                )
+                logger.debug(
+                    f"For number of interval = [{num_interval}], the trading day cut off is [{cut_off_trading_day}]"
+                )
+
+                logger.debug("Getting the training and testing data")
+                X_train, y_train, X_test, y_test = self._get_data(
+                    cut_off_trading_day,
+                    self.settings["model"]["network_model"]["prediction_num_days"],
+                    clean_scaled_data,
+                )
+                logger.info(f"Use LSTM model type {model_type} for al price prediction")
+                results_in_time_interval = self._run_lstm_model(
+                    model_type, X_train, y_train, X_test, y_test
+                )
+
+                logger.info(
+                    "Appending model prediction results with in the time interval to prediction results list"
+                )
+                lstm_y_pred_test.append(results_in_time_interval)
+
+            logger.info("Making prediction plots")
+            plt.plot(
+                np.arange(len(lstm_y_pred_test)),
+                lstm_y_pred_test,
+                label=f"model_structure_type_{model_type}",
+            )
+            plt.plot(np.arange(len(y_test)), y_test, label="actual")
+            plt.legend()
+            plt.title(f"LSTM Out-of-Sample Forecasts For Structure Type {model_type}")
+            Saver.save_plots(f"lstm_type_{model_type}_test_prediction_out_of_sample")
+            plt.clf()
+
+            logger.info("Give model performance metrics")
+            lstm_performance_test = calculate_performance_metrics(
+                y_test, lstm_y_pred_test
+            )
+
+            logger.info("Convert results to dataframe and save it")
+            test_results = pd.DataFrame.from_dict(
+                lstm_performance_test, orient="index"
+            ).reset_index()
+            test_results.columns = ["item", "value"]
+            Saver.save_csv(
+                test_results, f"lstm_type_{model_type}_test_results", "modelling"
+            )
+
+    def _run_lstm_model(self, model_type, X_train, y_train, X_test, y_test):
+        logger.debug(f"Getting the model architecture part [{model_type}]")
+        model = self._get_lstm_model(model_type)
+
+        logger.info("Training Model")
+        model.compile(optimizer="adam", loss="mse")
+        model.fit(
+            X_train,
+            y_train,
+            epochs=self.settings["model"]["network_model"]["epoches"],
+            batch_size=self.settings["model"]["network_model"]["batch_size"],
+            verbose=0,
         )
 
-        logger.debug("Running the LSTM model for difference structure")
-        for model_type in [1, 2, 3]:
-            logger.debug(f"Getting the model architecture part [{model_type}]")
-            model = self._get_lstm_model(model_type)
+        logger.info("Making predictions using the trained model and output results")
+        lstm_predictions_test = model.predict(X_test)
+        results_in_time_interval = [i[0] for i in lstm_predictions_test]
 
-            logger.debug("Training Model")
-            model.compile(optimizer="adam", loss="mse")
-            model.fit(
-                X_train,
-                y_train,
-                epochs=self.settings["model"]["network_model"]["epoches"],
-                batch_size=self.settings["model"]["network_model"]["batch_size"],
-                verbose=0,
-            )
+        return results_in_time_interval
 
     def _get_lstm_model(slef, type):
         if type == 1:
